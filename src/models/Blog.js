efe9c0f767ffb5b02e55acf85727f9b37c7ccc91
Paul Fontes <pgfon22@gmail.com>
2025-09-18 14:41:13 -0600

    first commit

---
+++ src/models/Blog.js
@@ -0,0 +1,16 @@
+
+
+
+
+export class Blog {
+    constructor(data) {
+
+        this.title = data.title
+        this.body = data.body
+        this.imgUrl = data.imgUrl
+        this.tags = data.tags
+        this.published = data.published
+        this.creatorId = data.creatorId
+        this.id = data.id
+    }
+}
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Example from '@/components/Example.vue';
+
+
+</script>
+
+<template>
+  This is the blog page
+</template>
+
+<style scoped lang="scss"></style>

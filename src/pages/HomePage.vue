ef828a17e92246ece27390dd7ff6ec53611a158c
Paul Fontes <pgfon22@gmail.com>
2025-09-18 14:48:58 -0600

    getting blogs

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,29 @@
 <script setup>
+import { AppState } from '@/AppState.js';
 import Example from '@/components/Example.vue';
+import { blogService } from '@/services/BlogService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { computed, onMounted } from 'vue';
 
+const blogs = computed(() => AppState.blog)
+
+onMounted(() => getBlogs())
+
+async function getBlogs() {
+  try {
+    blogService.getBlogs()
+  }
+  catch (error) {
+    Pop.error(error);
+    logger.log('You got an error getting the blogs', error)
+  }
+}
 
 </script>
 
 <template>
-  This is the blog page
+
 </template>
 
 <style scoped lang="scss"></style>
---
+++ src/services/BlogService.js
@@ -0,0 +1,16 @@
+import { logger } from "@/utils/Logger.js"
+import { api } from "./AxiosService.js"
+import { Blog } from "@/models/Blog.js"
+import { AppState } from "@/AppState.js"
+
+class BlogService {
+    async getBlogs() {
+        const response = await api.get('api/blogs')
+        const blogs = response.data.map((blogData) => new Blog(blogData))
+        AppState.blog = blogs
+
+    }
+
+}
+
+export const blogService = new BlogService()
